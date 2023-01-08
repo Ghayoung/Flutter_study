92dc56bacc1864ac1404745a8a32580b5dfc37c7
Nam Hayoung <33112915+Ghayoung@users.noreply.github.com>
2023-01-08 17:56:17 +0900

    20230108 study

--- lib/main_calculator.dart
+++ lib/main_calculator.dart
@@ -23,6 +23,9 @@ class _WidgetExampleState extends State<WidgetApp> {
   String sum = '';
   TextEditingController value1 = TextEditingController();
   TextEditingController value2 = TextEditingController();
+  List _buttonList = ['더하기', '빼기', '곱하기', '나누기'];
+  List<DropdownMenuItem<String>> _dropDownMenuItems = new List.empty(growable: true);
+  String? _buttonText;
 
   @override
   Widget build(BuildContext context) {
@@ -55,22 +58,50 @@ class _WidgetExampleState extends State<WidgetApp> {
                   style: ButtonStyle(backgroundColor: MaterialStateProperty.all(Colors.amber)),
                   onPressed: () {
                     setState(() {
-                      int result = int.parse(value1.value.text) + int.parse(value2.value.text);
+                      var value1Int = double.parse(value1.value.text);
+                      var value2Int = double.parse(value2.value.text);
+                      var result;
+                      if (_buttonText == '더하기') {
+                        result = value1Int + value2Int;
+                      } else if (_buttonText == '빼기') {
+                        result = value1Int - value2Int;
+                      } else if (_buttonText == '곱하기') {
+                        result = value1Int * value2Int;
+                      } else {
+                        result = value1Int / value2Int;
+                      }
                       sum = '$result';
                     });
                   },
                   child: Row(
                     children: <Widget>[
                       Icon(Icons.add),
-                      Text('더하기')
+                      Text(_buttonText!)
                     ],
                   )
                 ),
+              ),
+              Padding(
+                padding: EdgeInsets.all(15),
+                child: DropdownButton(items: _dropDownMenuItems, onChanged: (String? value) {
+                  setState(() {
+                    _buttonText = value;
+                  });
+                }, value: _buttonText,),
               )
             ],
           ),
         )
       )
     );
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    for(var item in _buttonList) {
+      _dropDownMenuItems.add(DropdownMenuItem(value: item, child: Text(item)));
+    }
+    _buttonText = _dropDownMenuItems[0].value;
   }
 }
