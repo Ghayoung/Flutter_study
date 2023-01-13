f3024dacbe28e3774e0bafc0299ef040545047e4
Nam Hayoung <33112915+Ghayoung@users.noreply.github.com>
2023-01-13 20:41:10 +0900

    20230113 study

--- lib/main_bookList.dart
+++ lib/main_bookList.dart
@@ -4,6 +4,7 @@
 
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http; // http 패키지 불러오기
+import 'dart:convert';
 
 void main() => runApp(MyApp());
 
@@ -23,6 +24,13 @@ class HttpApp extends StatefulWidget {
 
 class _HttpApp extends State<HttpApp> {
   String result = '';
+  List? data;
+
+  @override
+  void initState() {
+    super.initState();
+    data = new List.empty(growable: true);
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -32,19 +40,68 @@ class _HttpApp extends State<HttpApp> {
       ),
       body: Container(
         child: Center(
-          child: Text('$result'),
+          // data가 0일 때는 Text, 0이 아닐 때는 ListView
+          child: data!.length == 0
+          ? Text(
+            '데이터가 없습니다.',
+            style: TextStyle(fontSize: 20),
+            textAlign: TextAlign.center,
+          )
+          : ListView.builder(
+            itemBuilder: (context, index) {
+              return Card(
+                child: Container(
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: <Widget>[
+                      Image.network(
+                        data![index]['thumbnail'],
+                        height: 100,
+                        width: 100,
+                        fit: BoxFit.contain,
+                      ),
+                      Column(
+                        children: <Widget>[
+                          Container(
+                            width: MediaQuery.of(context).size.width - 150, // 지금 스마트폰의 화면 크기
+                            child: Text(
+                              data![index]['title'].toString(),
+                              textAlign: TextAlign.center,
+                            ),
+                          ),
+                          Text('저자: ${data![index]['authors'].toString()}'),
+                          Text('가격: ${data![index]['sale_price'].toString()}'),
+                          Text('판매중: ${data![index]['status'].toString()}'),
+                        ],
+                      )
+                    ],
+                  ),
+                ),
+              );
+            },
+            itemCount: data!.length
+          ),
         ),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: () async { // 비동기 처리
-          var url = 'http://www.google.com';
-          var response = await http.get(Uri.parse(url));
-          setState(() {
-            result = response.body;
-          });
+        onPressed: () {
+          // 비동기 처리
+          getJSONData();
         },
         child: Icon(Icons.file_download),
       ),
     );
+  }
+
+  Future<String> getJSONData() async {
+    var url = 'https://dapi.kakao.com/v3/search/book?target=title&query=doit';
+    var response = await http.get(Uri.parse(url),
+        headers: {"Authorization": "KakaoAK 82d0132fa34941d22a7755270052e623"});
+    setState(() {
+      var dataConvertedToJSON = json.decode(response.body);
+      List result = dataConvertedToJSON['documents'];
+      data!.addAll(result);
+    });
+    return response.body;
   }
 }
