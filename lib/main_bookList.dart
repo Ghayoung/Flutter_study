7494ce775610f2fcadae284fd74f25a558a18436
Nam Hayoung <33112915+Ghayoung@users.noreply.github.com>
2023-01-15 18:40:40 +0900

    20230115 study

--- lib/main_bookList.dart
+++ lib/main_bookList.dart
@@ -25,66 +25,95 @@ class HttpApp extends StatefulWidget {
 class _HttpApp extends State<HttpApp> {
   String result = '';
   List? data;
+  TextEditingController? _editingController;
+  ScrollController? _scrollController;
+  int page = 1;
 
   @override
   void initState() {
     super.initState();
     data = new List.empty(growable: true);
+    _editingController = new TextEditingController();
+    _scrollController = new ScrollController();
+
+/*  스크롤할 때마다 offset(현재 위치)를 검사해 maxScrollExtent보다 크거나 같고
+    스크롤 컨트롤러의 position에 정의된 범위를 넘어가지 않으면 목록의 마지막이라고 인식*/
+    _scrollController!.addListener(() {
+      if (_scrollController!.offset >=
+              _scrollController!.position.maxScrollExtent &&
+          !_scrollController!.position.outOfRange) {
+        print('bottom');
+        page++; // 리스트의 마지막일 때 실행
+        getJSONData();
+      }
+    });
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Http Example'),
+        title: TextField( // 검색창
+          controller: _editingController,
+          style: TextStyle(color: Colors.white),
+          keyboardType: TextInputType.text,
+          decoration: InputDecoration(hintText: '검색어를 입력하세요'),
+        ),
       ),
       body: Container(
         child: Center(
           // data가 0일 때는 Text, 0이 아닐 때는 ListView
           child: data!.length == 0
-          ? Text(
-            '데이터가 없습니다.',
-            style: TextStyle(fontSize: 20),
-            textAlign: TextAlign.center,
-          )
-          : ListView.builder(
-            itemBuilder: (context, index) {
-              return Card(
-                child: Container(
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.start,
-                    children: <Widget>[
-                      Image.network(
-                        data![index]['thumbnail'],
-                        height: 100,
-                        width: 100,
-                        fit: BoxFit.contain,
-                      ),
-                      Column(
-                        children: <Widget>[
-                          Container(
-                            width: MediaQuery.of(context).size.width - 150, // 지금 스마트폰의 화면 크기
-                            child: Text(
-                              data![index]['title'].toString(),
-                              textAlign: TextAlign.center,
+              ? Text(
+                  '데이터가 없습니다.',
+                  style: TextStyle(fontSize: 20),
+                  textAlign: TextAlign.center,
+                )
+              : ListView.builder(
+                  itemBuilder: (context, index) {
+                    return Card(
+                      child: Container(
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          children: <Widget>[
+                            Image.network(
+                              data![index]['thumbnail'],
+                              height: 100,
+                              width: 100,
+                              fit: BoxFit.contain,
                             ),
-                          ),
-                          Text('저자: ${data![index]['authors'].toString()}'),
-                          Text('가격: ${data![index]['sale_price'].toString()}'),
-                          Text('판매중: ${data![index]['status'].toString()}'),
-                        ],
-                      )
-                    ],
-                  ),
+                            Column(
+                              children: <Widget>[
+                                Container(
+                                  width: MediaQuery.of(context).size.width -
+                                      150, // 지금 스마트폰의 화면 크기
+                                  child: Text(
+                                    data![index]['title'].toString(),
+                                    textAlign: TextAlign.center,
+                                  ),
+                                ),
+                                Text(
+                                    '저자: ${data![index]['authors'].toString()}'),
+                                Text(
+                                    '가격: ${data![index]['sale_price'].toString()}'),
+                                Text(
+                                    '판매중: ${data![index]['status'].toString()}'),
+                              ],
+                            )
+                          ],
+                        ),
+                      ),
+                    );
+                  },
+                  itemCount: data!.length,
+                  controller: _scrollController,
                 ),
-              );
-            },
-            itemCount: data!.length
-          ),
         ),
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
+          page = 1;
+          data!.clear(); // 버튼을 누를 때마다 기존 내용을 지우고 페이지 초기화
           // 비동기 처리
           getJSONData();
         },
@@ -94,7 +123,8 @@ class _HttpApp extends State<HttpApp> {
   }
 
   Future<String> getJSONData() async {
-    var url = 'https://dapi.kakao.com/v3/search/book?target=title&query=doit';
+    var url = 'https://dapi.kakao.com/v3/search/book?'
+        'target=title&page=$page&query=${_editingController!.value.text}';
     var response = await http.get(Uri.parse(url),
         headers: {"Authorization": "KakaoAK 82d0132fa34941d22a7755270052e623"});
     setState(() {
