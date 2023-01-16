912be656beac6fb3f16009fb7b42ababd51d7b92
Nam Hayoung <33112915+Ghayoung@users.noreply.github.com>
2023-01-16 21:03:51 +0900

    20230116 study

---
+++ lib/main_largeFileDownload.dart
@@ -0,0 +1,19 @@
+/*
+이미지 파일 내려받기 main
+ */
+
+import 'package:flutter/material.dart';
+import 'largeFileMain.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: LargeFileMain(),
+    );
+  }
+}
